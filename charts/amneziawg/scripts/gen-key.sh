2cb7e82973fab98b54c254b99fad1274e6505a2c
Mikolaj Sobolewski <51751942+mikolajsobolewski@users.noreply.github.com>
2024-08-29 14:05:02 +0300

    Update charts/amneziawg/scripts/gen-key.sh
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- charts/amneziawg/scripts/gen-key.sh
+++ charts/amneziawg/scripts/gen-key.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-kubectl --namespace $RELEASE_NAMESPACE create secret generic $SECRET_NAME --from-literal=privatekey=$(awg genkey)
+kubectl --namespace "$RELEASE_NAMESPACE" create secret generic "$SECRET_NAME" --from-literal=privatekey="$(awg genkey)"
